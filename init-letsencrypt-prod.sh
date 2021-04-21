171c5147df29c5e2e771149060020ebfef6b632c
Pratik Aher <pratik88aher@gmail.com>
2021-04-21 16:47:41 +0530

    parent changes

--- init-letsencrypt-prod.sh
+++ init-letsencrypt-prod.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(feedback.termer.tingtun.no www.feedback.termer.tingtun.no)
+domains=(feedback.titi.tingtun.no www.feedback.titi.tingtun.no)
 # domains=(feedback.titi.tingtun.no)
 rsa_key_size=4096
 data_path="./data/certbot"
